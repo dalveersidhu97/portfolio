a8c00881cdf2ef776315a6dd5cb132fe640b4ea0
Dalveer Singh <dalveersidhu97@gmail.com>
2022-03-04 20:55:58 -0500

    fixed side navs

--- Components/Main.tsx
+++ Components/Main.tsx
@@ -1,10 +1,15 @@
 
-import React, { FunctionComponent, useContext } from "react";
+import React, { FunctionComponent, useContext, useEffect } from "react";
 import styles from './Layout.module.css';
 import cStyles from '../styles/common.module.css';
 import { MenuContext } from "../context/MenuContext";
 
 const MainSection:FunctionComponent = (props) => {
+
+    useEffect(()=> {
+        window.document.addEventListener('click', () => {context?.hideAll()})
+    });
+    
 
     const context = useContext(MenuContext);
     return <main  onClick={() => {context?.hideAll()}}
